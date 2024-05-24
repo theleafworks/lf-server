1b5ce7c0f697732a5f3d015b7273d0f62465e431
Vismay <vismayyk4444@gmail.com>
2024-05-24 23:11:30 +0530

    added customized email for vendors

--- middleware/vendorMiddleWare.js
+++ middleware/vendorMiddleWare.js
@@ -2,8 +2,9 @@ import VendorModel from "../models/VendorModel.js";
 
 // checking the domain before the vendor can access the services
 export const checkVendorAccess = async (req, res, next) => {
-    const vendor = await VendorModel.findOne({ domain: req.hostname });
-    console.log("New request received for vendor : ",vendor?.hostname ?? req.hostname);
+    
+    const vendor = await VendorModel.findOne({ domain: req.origin });
+    console.log("New request received for vendor : ",vendor?.hostname ?? req.origin);
 
     if (vendor){
         req.vendor = vendor;
